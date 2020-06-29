fba5dfde73d26f5ba16c7d9c0beb667713832161
João Vitor Archanjo <jvitorarchanjo@gmail.com>
2020-06-29 15:31:47 -0300

    Criação pop up

---
+++ objgilzinho.js
@@ -0,0 +1,7 @@
+const salao = {
+    nome: "Salão Gilberto Gil",
+    andar: 2,
+    informacao: "O salão foi pensado no nome do cantor e compositor Gilberto Gil como forma de homenagem e referências as atiidades que acontecem no local."
+}
+
+    document.getElementById('gilzinho').innerHTML = `${salao.nome} fica no ${salao.andar e ${salao.informacao}}°`
